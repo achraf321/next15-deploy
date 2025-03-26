c75bf76229ecf5888d7d5660fb3f157c2d9c4294
Ashraf <ouzala30@gmail.com>
2025-03-26 01:00:09 +0100

    Fixed stuff

--- app/create-user/page.tsx
+++ app/create-user/page.tsx
@@ -4,42 +4,43 @@ import { useMutation } from '@tanstack/react-query'
 import { useRouter } from 'next/navigation'
 import React, { useEffect } from 'react'
 
+async function GetProfile() {
+    const response = await fetch("/api/create-user", {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json"
+        }
+    })
 
-async function GetProfile () {
-const response = await fetch("/api/create-user",{
-    method : "POST" ,
-    headers : {
-        "Content-type" : "application/json"
-    }
-})
-
-const data = await response.json()
-return data
+    const data = await response.json()
+    return data
 }
 
 const Page = () => {
-const {isSignedIn , isLoaded} = useUser()
-const router = useRouter()
-const {mutate , isPending} = useMutation({
-    mutationFn : GetProfile,
-    onSuccess : () => {
-        router.push("/executive")
-    },
-    onError : (error) => {
-        console.log(error)
-    }
-})
-useEffect(() => {
-    if(isLoaded && isSignedIn && !isPending){
-        mutate()
-    }
-}, [isLoaded , isSignedIn])
+    const { isSignedIn, isLoaded } = useUser()
+    const router = useRouter()
+    const { mutate, isPending } = useMutation({
+        mutationFn: GetProfile,
+        onSuccess: () => {
+            router.push("/executive")
+        },
+        onError: (error) => {
+            console.log(error)
+        }
+    })
 
-  return (
-    <div>
-      Procession Sign In ...
-    </div>
-  )
+    useEffect(() => {
+        if (isLoaded && isSignedIn && !isPending) {
+            mutate()
+        }
+    }, [isLoaded, isSignedIn, isPending])  // Added isPending to the dependency array
+
+    return (
+        <div>
+            Processing Sign In ...
+        </div>
+    )
 }
 
 export default Page
+
